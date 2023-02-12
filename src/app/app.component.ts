af023a6efe9876e9627539ebc2badfd8556e0fa7
MariamikGhazaryan <mariamikghazaryan1997@gmail.com>
2023-02-12 15:50:50 +0400

    Without Web Workers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'web-workers';
+  letters = '0123456789ABCDEF';
+  color = 'red';
+  sum = 0
+
+  onCalculateSum() {
+    this.sum = 0;
+    for (let i = 0; i < 1000000000; i++) {
+      this.sum += i;
+    }
+  }
+
+  onBackGroundChange() {
+    this.color = "#";
+    for (let i = 0; i < 6; i++) {
+      this.color += this.letters[Math.floor(Math.random() * 16)];
+    }
+  }
 }
